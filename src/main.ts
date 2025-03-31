73689830970b19e046888d2a69571d6d533c5e17
jcbkdev <kubkon134@gmail.com>
2025-03-31 14:09:17 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import adminRoutes from "./routes/admin";
 import apiRoutes from "./routes/api";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
